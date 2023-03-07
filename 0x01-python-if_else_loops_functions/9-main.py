d3bcba2953543b49a22d54cb6d2d06a286f9fa61
praise obah <obahsomto@gmail.com>
2023-03-07 15:28:55 -0800

    initial commit

---
+++ 0x01-python-if_else_loops_functions/9-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+print_last_digit = __import__('9-print_last_digit').print_last_digit
+
+print_last_digit(98)
+print_last_digit(0)
+print_last_digit(54)
+print_last_digit(57)
+b = print_last_digit(-65)
+r = print_last_digit(-1024)
+print(r)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+def print_last_digit(number):
+    number = abs(number) % 10
+    print("{:d}".format(number), end="")
+    return number
