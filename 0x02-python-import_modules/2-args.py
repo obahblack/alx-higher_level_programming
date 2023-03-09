687d0281b137a1f86a0ed23d99a6c155288b50e6
praise obah <obahsomto@gmail.com>
2023-03-09 06:06:21 -0800

    argument fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     if len(argv) == 2:
         print("{:d} argument:\n{:d}: {}".format(len(argv) - 1, len(argv) - 1, argv[1]))
     if len(argv) > 2:
-        print("{:d} arguments:".format(len(argv)-1))
+        print("{:d} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{:d}: {}".format(i, argv[i]))
