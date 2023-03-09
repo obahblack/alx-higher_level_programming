06a90dce0981ac733a27d80393b2e36e5f5c284c
praise obah <obahsomto@gmail.com>
2023-03-09 06:02:27 -0800

    argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("{:d} arguments.".format(len(argv) - 1))
+    if len(argv) == 2:
+        print("{:d} argument:\n{:d}: {}".format(len(argv) - 1, len(argv) - 1, argv[1]))
+    if len(argv) > 2:
+        print("{:d} arguments:".format(len(argv)-1))
+        for i in range(1, len(argv)):
+            print("{:d}: {}".format(i, argv[i]))
