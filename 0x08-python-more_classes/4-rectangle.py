d6d18c42cbaa7909fc85c626f504c9f3be351d9a
praise obah <obahsomto@gmail.com>
2023-04-04 05:44:17 -0700

    correction

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -42,8 +42,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -52,20 +51,19 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """ setter to get value """
+        """ Setter to get value """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
         """ Calculates and return the area of the shape """
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """ calulates the perimeter of the shape """
+        """ Calulates the perimeter of the shape """
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
@@ -74,12 +72,9 @@ class Rectangle:
         """ Prints rectangle """
         if self.__width == 0 or self.__height == 0:
             return ""
-        pic = ""
-        for i in range(self.__height):
-            row = "#" * self.__width
-            pic += row + "\n"
+        pic = "\n".join(["#" * self.__width for rows in range(self.__height)])
         return pic
 
     def __repr__(self):
-        """ string representation to create new instance """
+        """ String representation to create new instance """
         return "Rectangle({:d}, {:d})".format(self.width, self.height)
