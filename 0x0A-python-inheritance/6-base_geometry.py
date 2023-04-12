7ecde080d4624ef2df17ec6a94e671bfc60ccc28
praise obah <obahsomto@gmail.com>
2023-04-11 21:45:59 -0700

    initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+
+Contains empty class BaseGeometry
+with public instance method area
+"""
+
+
+class BaseGeometry:
+    """
+    Methods:
+    area(self)
+    """
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
