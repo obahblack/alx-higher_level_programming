349f94ff5decc42bbb8adbf4583dd0cbd11c2abe
praise obah <obahsomto@gmail.com>
2023-04-12 12:17:59 -0700

    commit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,6 @@ Module a function tht writes astring to a tsxt file
 
 def write_file(filename="", text=""):
     """write a string to a text file (UTF8) and returns the number of characters"""
+
     with open(filename, mode='w', encoding='utf-8') as f:
         return f.write(text)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A function that returns an object reps by a Json
+"""
+
+
+import json
+
+def from_json_string(my_str):
+    """from json string to object"""
+    return json.loads.(my_str)
