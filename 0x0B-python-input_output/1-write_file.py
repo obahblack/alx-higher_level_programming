a90deb1e8e4340f303f34ac50979978a8a1d9fe4
praise obah <obahsomto@gmail.com>
2023-04-12 11:12:46 -0700

    write and append

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module a function tht writes astring to a tsxt file
+"""
+
+
+def write_file(filename="", text=""):
+    """write a string to a text file (UTF8) and returns the number of characters"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return f.write(text)
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A function that appends a string at the end of a text file(UTF8)
+"""
+
+
+def append_write(filename="", text=""):
+    """append a string at the end of a text file"""
+    with open(filename, mode='a' encoding="utf-8") as f:
+        return f.write(text)
