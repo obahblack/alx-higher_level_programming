8868fdcb120277c37d996959570247384b2a0a66
praise obah <obahsomto@gmail.com>
2023-04-12 11:15:41 -0700

    correction of append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,5 +6,6 @@ A function that appends a string at the end of a text file(UTF8)
 
 def append_write(filename="", text=""):
     """append a string at the end of a text file"""
-    with open(filename, mode='a' encoding="utf-8") as f:
+
+    with open(filename, mode='a', encoding="utf-8") as f:
         return f.write(text)
