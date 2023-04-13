12a9ba0190548128775578996a8b056defdfe88f
praise obah <obahsomto@gmail.com>
2023-04-13 13:50:36 -0700

    initial commit

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module - 9-student.py
+With public instance attributes
+"""
+
+
+class Student():
+    """
+    Public Attributes:
+       first_name
+       last_name
+       age
+
+    Public methods
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes student with full name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Return a dictionary representation of a 
+        Student instancce
+        """
+        return self.__dict__
