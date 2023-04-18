18adc9df3b0aa1bf675fc120259530773cc9844b
Praise Obah <obahblack2k18@gmail.com>
2023-04-18 04:57:35 -0700

    correction commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -127,7 +127,7 @@ class Rectangle(Base):
         """
         print("\n" * self.__y, end="")
         for i in range(self.__height):
-            print(" " * self__x + "#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Prints [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
