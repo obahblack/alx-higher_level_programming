f4bdc21e3d5292681037513ab724ef13943ca387
Praise Obah <obahblack2k18@gmail.com>
2023-04-21 02:08:45 -0700

    test files

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -57,3 +57,117 @@ class TestBase(unittest.TestCase):
         with self.assertRaises(AttributeError):
             print(Base.__nb_objects)
             print(Base.nb_objects)
+
+    """Test args given"""
+    def test_invalid_args(self):
+        """Test too many args given throws error"""
+        with self.assertRaises(TypeError):
+            Base(50, 50)
+
+    """Test class"""
+    def test_class(self):
+        """Test class created is indeed Base"""
+        self.assertTrue(Base(100), self.__class__ == Base)
+
+    """Test python obj to JSON"""
+    def test_to_json_string(self):
+        """Test dict given translates into JSON string"""
+        d0 = {"id": 1, "width": 2, "height": 3, "x": 4, "y": 5}
+        d1 = {"id": 6, "width": 7, "height": 8, "x": 9, "y": 10}
+        strd01 = Base.to_json_string([d0, d1])
+        self.assertTrue(type(d0) == dict)
+        self.assertTrue(type(strd01) == str)
+        self.assertTrue(strd01, [{"id": 1, "width": 2, "height": 3, "x": 4, "y": 5}, {"id": 6, "width": 7, "height": 8, "x": 9, "y": 10}])
+
+    def test_none_to_json_string(self):
+        """Test no dict given translates into JSON string of empty dict"""
+        d2 = None
+        strd2 = Base.to_json_string([d2])
+        self.assertTrue(type(strd2) == str)
+        self.assertTrue(strd2, "[]")
+
+    def test_empty_to_json_string(self):
+        """Test empty dict given translates into JSON string of empty dict"""
+        d3 = dict()
+        strd3 = Base.to_json_string([d3])
+        self.assertTrue(len(d3) == 0)
+        self.assertTrue(type(strd3) == str)
+        self.assertTrue(strd3, "[]")
+
+    """Test Json to python object"""
+    def test_from_json_string(self):
+        """Test Json string translates into python dict"""
+        s0 = '[{"id": 1, "width": 2, "height": 3, "x": 4, "y": 5},\
+                {"id": 6, "width": 7, "height": 8, "x": 9, "y": 10}]'
+        strs0 = Base.from_json_string(s0)
+        self.assertTrue(type(s0) == str)
+        self.assertTrue(type(strs0[0] == list)
+        self.assertTrue(type(strs0, [{"id": 1, "width": 2, "height": 3, "x": 4, "y": 5}, {"id": 6, "width": 7, "height": 8, "x": 9, "y": 10}])
+        self.assertTrue(strs0[0], {"id": 1, "width": 2, "height": 3, "x": 4, "y": 5})
+
+    def test_from_none_json_string(self):
+        """Test no json string tranlates into empty python dict"""
+        s2 = None
+        str2 = Base.from_json_string(s2)
+        self.assertTrue(type(strs2) == list)
+        self.assertTrue(strs2 == [])
+
+    def test_from__empty_json_string(self):
+        """Test no Json string translates into empty python dict"""
+        s3 = ""
+        strs3 = Base.from_json_string(s3)
+        self.assertTrue(type(strs3) == list)
+        self.assertTrue(strs3 == [])
+
+    """Test creating instance from dictionary"""
+    def test_create(self):
+        """Test transferring attribute dictionary to another instance"""
+        r = Rectangle(3, 5, 1, 2, 99)
+        rdic = r.to__dictionary()
+        r2 = Rectangle.create(**rdic)
+        self.assertEqual(str(r), '[Rectangle] (99) 1/2 - 3/5')
+        self.assertEqual(str(r2), '[Rectangle] (99) 1/2 - 3/5')
+        self.assertIsNot(r, r2)
+
+    """Test saving JSON string repr of dict to class specific file"""
+    def test_save_to_file(self):
+    """Test save to file"""
+    r = Rectangle(10, 7, 2, 8, 99)
+    r2 = Rectangle(2, 4, 2, 2, 98)
+    Rectangle.save_to_file([r, r2])
+    with open("Rectangle.json", "r") as file:
+        self.assertEqual(json.dumps([r.to_dictionary(), r2.to_dictionary()]), file.read())
+
+    def test_save_none_to_file(self):
+        """Test save None to file"""
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual('[]', file.read())
+
+    """Test loading list of instances from Json string repr of dict in file"""
+    def test_load_from_file(self):
+        """Test load from file"""
+        r = Rectangle(10, 7, 2, 8, 99)
+        r2 = Rectangle(2, 4, 2, 2, 98)
+        Rectangle.save_to_file([r, r2])
+        recs = Rectangle..load_from file()
+        self.assertEqual(len(recs), 2)
+        for k, v in enumerate(recs):
+            if k == 0:
+                self.assertEqual(str(v), '[Rectangle] (99) 2/8 - 10/7')
+            if k == 1:
+                self.assertEqual(str(v), '[Rectangle] (98) 2/2 - 2/4')
+
+    def test_load_from_non_file(self):
+        """Test load from None file"""
+        Rectangle.save_to_file(None)
+        recs = Rectangle.load_from_file()
+        self.assertEqual(type(recs), list)
+        self.assertEqual(len(recs), 0)
+
+    def test_load_from_empty_file(self):
+        """Test load from empty file"""
+        Rectangle.save_to_file([])
+        recs = Rectangle.load_from_file()
+        self.assertEqual(type(recs), list)
+        self.assertEqual(len(recs), 0)
