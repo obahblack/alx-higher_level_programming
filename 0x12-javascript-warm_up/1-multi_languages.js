3afcc059a5415a5a82ec350a1b829caceea9f53b
Praise Obah <obahsomto@gmail.com>
2024-01-08 12:17:14 -0800

    correction

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 // printing things on different line
-console.log("C is fun")
-console.log("Python is cool")
-console.log("JavaScript is amazing")
+console.log("C is fun\nPython is cool\nJavaScript is amazing")
