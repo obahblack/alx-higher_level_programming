30033feccdd122933d11a443bb127660c836f166
Praise Obah <obahsomto@gmail.com>
2024-01-13 01:30:54 -0800

    filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+filters and return states startin with 'N'
+parameters given to script: username, password, database
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ = "__main__":
+    
+    # connect to database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+    cursor.close()
+    db.close()
