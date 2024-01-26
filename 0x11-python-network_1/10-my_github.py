955ceb7cfd071061f41eb31151bfb74595df59f6
Praise Obah <obahsomto@gmail.com>
2024-01-26 14:28:44 -0800

    api correction

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,16 +3,11 @@
 given letter pattern as param to be search val of request; print Star War names
 usage: ./9-starwars.py [letter pattern to match names]
 """
-from sys import argv
 import requests
-
+from sys import argv
 
 if __name__ == "__main__":
-    url = 'https://swapi.co/api/people'
-    param = {'search': argv[1]}
-    r = requests.get(url, params=param)
-
-    matching_ppl = r.json()
-    print("Number of results: {}".format(matching_ppl.get('count')))
-    for person in matching_ppl.get('results'):
-        print(person.get('name'))
+    req = requests.get('https://api.github.com/user',
+                       auth=(argv[1], argv[2]))
+    json_format = req.json()
+    print(json_format.get('id'))
