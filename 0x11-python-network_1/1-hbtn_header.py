250e383f3ed33c78b1668cbd1816944b0c7cbfef
Praise Obah <obahsomto@gmail.com>
2024-01-26 10:05:10 -0800

    urllib

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+given URL as parameter, fetch URL and display value from response header
+usage: ./1-hbtn_header https://intranet.hbtn.io
+"""
+from sys import argv
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.getheader('x-Request-Id'))
