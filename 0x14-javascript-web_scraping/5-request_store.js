6baa7fc6349cd8ce69bbbc2dd63c38bf03b7c034
Praise Obah <obahsomto@gmail.com>
2024-02-20 07:12:13 -0800

    web scrapping

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// a script to print the contents of a webpage and stores it in a file.
+let url = process.argv[2];
+let filename = process.argv[3];
+const fs = require('fs');
+const request = require('request');
+
+request(url, funtion (err, response, body) {
+	if (err) {
+		console.log(err);
+	} else {
+		fs.writeFile(filename, body, 'utf8');
+	}
+});
